3034423b0114d67d8263d688f0bec1bdd701afee
francisco.n.azevedo@gmail.com <francisco.n.azevedo@gmail.com>
2026-04-21 12:01:31 +0100

    n topggles goes to 50

--- Tasks/learn_to_fixate_ego_allo/Arduino/src/main.cpp
+++ Tasks/learn_to_fixate_ego_allo/Arduino/src/main.cpp
@@ -55,7 +55,7 @@ unsigned long reward_tone_freq = 1750;
 // Parameters for current pumps and pokes
 unsigned long reward_valve_dur = 2000; // more than enough for pump to push water
 unsigned long reward_pump_toggle_dur = 3; // ms
-int targetToggles = 71; // Total number of toggles to perform , double of pump steps
+int targetToggles = 50; // Total number of toggles to perform , double of pump steps
 unsigned long grace_period = 100; // ms to avoid poke fluctuations
 
 // odors
